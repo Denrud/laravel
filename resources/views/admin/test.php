e46be2de9bf97bc0a8ed3b99ce2ef1917c8b954b
DRUD <rudikdenis@gmail.com>
2020-06-18 15:21:42 +0300

    add few files

---
+++ resources/views/admin/test.php
@@ -0,0 +1,26 @@
+<?php include $_SERVER['DOCUMENT_ROOT']. "/admin/partials/_header.php"; // подключение header  ?>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_navbar.html -->
+      <?php include $_SERVER['DOCUMENT_ROOT']. "/admin/partials/_navbar.php"; // подключение навигационной панели (горизонтальная панель) ?>
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        <?php include $_SERVER['DOCUMENT_ROOT']. "/admin/partials/_sidebar.php"; // подключение боковой панели меню админки  ?>
+        <!-- partial:partials/_sidebar.html end-->
+        <!-- partial -->
+        <div class="main-panel">
+          <div class="content-wrapper">
+              <div class="d-xl-flex align-items-center">
+                <h2 class="text-dark font-weight-bold mb-2 mr-2"> Обзорная панель </h2>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item  active" aria-current="page"><a href="/admin">Главная</a></li>
+                        </ol>
+                    </nav>
+                </div>
+          </div>
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.php -->
+          <?php include $_SERVER['DOCUMENT_ROOT']. "/admin/partials/_footer.php"; // подключение footer ?>
+          <!-- partial -->
